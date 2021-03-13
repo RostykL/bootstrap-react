32efce2c9c6ec2d987f0ad2532f1b37a09d1618e
Rostyk Lukavyi <rostyk.lukavyi842@gmail.com>
2021-03-13 22:07:50 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Header from './components/header/header'
+import SideBar from './components/sidebar/sidebar'
+
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+
+
+import {useEffect, useState} from "react";
 
 function App() {
+  const [show, setShow] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	  <>
+		<Row>
+		  <Col className={`sidebar bg-light ${!show ? 'hideSideBar' : 'showSideBar'}`}>
+			<SideBar/>
+		  </Col>
+		  <Col className={"content"}>
+			<Header setShow={setShow} show={show}/>
+			<Container fluid>
+			  <h1 className={"mt-4"}>Simple Sidebar</h1>
+			  <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed
+				on larger screens. When toggled using the button below, the menu will change.</p>
+			  <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is
+				optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which
+				will toggle the menu when clicked.</p>
+
+			</Container>
+		  </Col>
+		</Row>
+	  </>
   );
 }
 
